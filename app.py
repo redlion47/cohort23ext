2303ea1d13e52a4c650d4ffbf2f6902fce9e1171
redlion47 <katoo.eric@gmail.com>
2017-10-22 23:51:22 +0300

    [fixes]reconfigured how the app.py runs

--- app.py
+++ app.py
@@ -86,6 +86,4 @@ def addedRecipe():
 	return render_template('designs/UI/RecipeList.html', result = category)
 
 if __name__ == '__main__':
-     app.debug = True
-     port = int(os.environ.get("PORT", 5000))
-     app.run(host='0.0.0.0', port=port)
+     app.run()
